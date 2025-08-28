546865b572fdac3ab64b55943b12441d47508e46
MukioXun <3045859462@qq.com>
2025-08-28 15:33:29 +0800

    brk main ver.1

--- js/index.js
+++ js/index.js
@@ -50,24 +50,24 @@ root.innerHTML = `
         <p>Unified Mixture of Experts for Enhanced AI Performance</p>
       </div>
       <div class="footer-section">
-        <h4>Links</h4>
-        <ul>
-          <li><a href="#" onclick="showIntroduction()">Introduction</a></li>
-          <li><a href="#" onclick="showShowcase()">Showcase</a></li>
-          <li><a href="https://github.com/your-repo" target="_blank">GitHub Repository</a></li>
-        </ul>
+        <div class="footer-links">
+            <div class="footer-icons">
+              <a href="https://github.com/HITsz-TMG/UMOE-Scaling-Unified-Multimodal-LLMs" target="_blank" class="footer-link" title="GitHub">
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
+                  <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
+                </svg>
+              </a>
+              <a href="https://huggingface.co/Uni-MoE" target="_blank" class="footer-link" title="Hugging Face">
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
+                  <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-3.5-6.5c0-.83.67-1.5 1.5-1.5s1.5.67 1.5 1.5-.67 1.5-1.5 1.5-1.5-.67-1.5-1.5zm5 0c0-.83.67-1.5 1.5-1.5s1.5.67 1.5 1.5-.67 1.5-1.5 1.5-1.5-.67-1.5-1.5z"/>
+                </svg>
+              </a>
+            </div>
+            <div class="footer-copyright">
+              <p>&copy; 2025 Uni-MoE</p>
+            </div>
+          </div>
       </div>
-      <div class="footer-section">
-        <h4>Connect</h4>
-        <ul>
-          <li><a href="https://github.com/your-repo" target="_blank">GitHub</a></li>
-          <li><a href="mailto:contact@unimoe.com">Contact</a></li>
-          <li><a href="#" onclick="showIntroduction()">Documentation</a></li>
-        </ul>
-      </div>
-    </div>
-    <div class="footer-bottom">
-      <p>&copy; 2025 Uni-MoE Project. All rights reserved.</p>
     </div>
   </footer>
 `;
